496aae6a1a790ffde7823ea1494e5ff73d06e1e3
anujk <anujkaushik1512@gmail.com>
2021-08-19 12:50:20 +0530

    Variables in PHP

---
+++ Basics/Variables.php
@@ -0,0 +1,11 @@
+<?php
+
+$name="Anuj kaushik";
+$income="100000";
+
+echo "My name is $name and income is $income <br><br><br><br><br><br><br><br><br>"; //alag bhi use kr skte hai or ese bhi
+echo "<br>";                               //BREAK STATEMENT MTLB SPACEBAR CHRDNE KE LIYE
+echo"Have i told you my income is $income";
+
+?>
+
