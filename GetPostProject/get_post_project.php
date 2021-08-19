4b8545b865455e9a3067f47d2772155597c89f2b
anujk <anujkaushik1512@gmail.com>
2021-08-20 00:03:58 +0530

    CRUD Project in PHP

---
+++ GetPostProject/get_post_project.php
@@ -0,0 +1,147 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$database="Project";
+
+$conn=mysqli_connect($servername,$username,$password,$database);
+if(!$conn){
+    echo "connection failed";
+}
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $title=$_POST["title"];
+    $description=$_POST["desc"];
+
+    $sql="INSERT INTO notes (title,description)
+                            VALUES('$title','$description')";
+
+      $result=mysqli_query($conn,$sql);
+      
+
+}
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script
+  src="https://code.jquery.com/jquery-3.5.1.js"
+  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+    <script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+   
+
+    <script>
+    $(document).ready( function () {
+    $('#myTable').DataTable();    //iss purre code mei ksiki id mytale hai issleye b html mei cha ge krnge id lgakr
+} );
+    </script>
+
+    <title>Project</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">PHP Project</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+    <div class="container my-4">
+    <h2>Add some notes</h2>
+    <form action="/anujphp/crudproject/project.php" method="post">
+  <div class="mb-3">
+    <label for="title" class="form-label">Title</label>
+    <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3">
+    <label for="desc" class="form-label">Description</label>
+    <input type="text" class="form-control" id="desc" name="desc">
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+
+    <div class="container">
+    
+    <table class="table" id="myTable">
+  <thead>
+    <tr>
+     
+      <th scope="col">SNO</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+    </tr>
+  </thead>
+  <tbody>
+  
+  <?php
+
+  $sql = "SELECT * FROM notes";
+  $result=mysqli_query($conn,$sql);
+
+
+  while($row=mysqli_fetch_assoc($result)){
+    echo "<tr>
+    <th scope='row'>".$row['sno']."</th>
+    <td>".$row['title']."</td>
+    <td>".$row['description']."</td>
+   
+  </tr> ";
+  }
+
+
+  ?>
+
+   
+   
+  </tbody>
+</table>
+
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
