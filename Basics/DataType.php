e5adeaea84aec9a888f7372a69d9c21ca0f83965
anujk <anujkaushik1512@gmail.com>
2021-08-19 13:00:44 +0530

    Data Types in PHP

---
+++ Basics/DataType.php
@@ -0,0 +1,45 @@
+
+//INTEGER=
+
+<?php
+
+$income=100;
+$income1=200;
+$income2=$income;
+
+echo "$income+$income1<br>";
+echo $income+$income1+$income2;
+
+?>
+
+//BOOLEAN
+
+<?php
+
+$moni=true;
+$anuj=false;
+
+echo "<br>";
+echo var_dump($moni);
+echo var_dump($anuj);
+?>
+
+//ARRAY
+
+<?php
+
+echo "<br>";
+
+$friends = array("anuj","nishant","neeraj","kamal");
+echo var_dump($friends);
+echo "<br>";
+echo $friends[0];
+
+?>
+
+<?php
+echo "<br>";
+$test = "anuj kaushik";
+$test = "anuj";
+echo $test;
+?>
