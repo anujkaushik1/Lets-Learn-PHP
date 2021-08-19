85c255337d4beef9c4e801e48001da725dfa5950
anujk <anujkaushik1512@gmail.com>
2021-08-19 23:55:02 +0530

    Var Dump Function in PHP

---
+++ Functions/vardump.php
@@ -0,0 +1,19 @@
+<?php
+echo var_dump(100);    
+?>
+
+//OUTPUT = int(100)
+
+<?php
+echo "<br>";
+echo var_dump("100");   
+?>
+
+//OUTPUT = string("100");
+
+<?php
+//echo "<br><br>  "
+//echo var_dump(name);
+?>
+
+//OUTPUT = error */
