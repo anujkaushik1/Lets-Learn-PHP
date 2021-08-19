9c37f37a5ec366600a015069f125eee24ff96a01
anujk <anujkaushik1512@gmail.com>
2021-08-19 12:59:17 +0530

    Operators in PHP

---
+++ Basics/Operators.php
@@ -0,0 +1,39 @@
+<?php
+$a= 20;
+$b=30;
+echo "$a+$b";    //output= 20+30
+?>
+
+<?php
+echo "<br>";
+$a= 20;
+$b=30;
+echo $a+$b;    //output= 50
+?>
+
+<?php
+echo "<br";
+echo "kldjkskkrfkjfjk";
+$a=10;
+$b=20;
+echo "addition is ".($a+$b);  //output= addition is 50
+?>
+
+//comaprion operator=
+
+<?php
+echo "<br> <br>";
+$x=10;
+$y=20;
+
+echo var_dump($x==$y);
+echo var_dump($x>$y);
+echo var_dump($x<$y);
+echo "<br>";
+$a=20;
+$b=20;
+
+echo var_dump($a<=$b);
+
+?>
+
