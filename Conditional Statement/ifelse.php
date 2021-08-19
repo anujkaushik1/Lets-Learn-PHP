89f05c53f1c54f471901779ebb4695b3a00cf583
anujk <anujkaushik1512@gmail.com>
2021-08-19 23:42:36 +0530

    Conditional Statements

---
+++ Conditional Statement/ifelse.php
@@ -0,0 +1,31 @@
+<?php
+$a=20;
+$b=30;
+
+
+
+if($a==$b){
+    echo "equal";
+
+}
+else{
+    echo "not equal";
+}
+?>
+
+<?php
+echo "<br>";
+$a=20;
+$b=30;
+
+if($a==$b){
+    echo "yes";
+}
+elseif($a<=$b){
+    echo "a is smaller";
+}
+else{
+    echo "a is bigger";
+}
+
+?>
